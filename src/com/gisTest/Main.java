94dfdbd22995f8a4d682e4f5592fcb36ae4e7d11
Phoebus <1634372267@qq.com>
2018-08-05 13:14:43 +0800

    IDEA整合GitHub测试 输出Hello Git和for循环

--- src/com/gisTest/Main.java
+++ src/com/gisTest/Main.java
@@ -3,6 +3,9 @@ package com.gisTest;
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello Git");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i);
+        }
     }
 
 }
